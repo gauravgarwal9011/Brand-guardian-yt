0b54ef40f9bc2e170c07ec4a74c41560d06b8422
Gaurav Garwal <gauravgarwal9011@gmail.com>
2026-02-13 06:20:35 +0000

    updated nodes

---
+++ backend/src/graph/nodes.py
@@ -0,0 +1,11 @@
+# nodes.py
+import json
+import os
+import logging
+import re
+from typing import Dict, Any, List
+
+from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
+from langchain_community.vectorstores import AzureSearch
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import SystemMessage, HumanMessage
